ea884dc839dd580a9418570be74283a48d8e9402
Peter Krivan <krivan.peter@outlook.com>
2019-05-07 11:18:05 +0000

    home page navbar sticky to top with changing styles on large screens

---
+++ static/js/class_swapper.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $(document).scroll(function() {
+        if ($(window).scrollTop() === 0) {
+            $('.navbar').addClass('navbar-transparent');
+            $('.navbar').removeClass('navbar-white');
+            $('.navbar-brand').addClass('logo');
+            $('.navbar-brand').removeClass('logo-transparent');
+            $('.nav-item a').addClass('white-text');
+            $('.nav-item a').removeClass('black-text');
+            $('.nav-link i').addClass('white-text');
+            $('.nav-link i').removeClass('black-text');
+        }
+        else {
+            $('.navbar').addClass('navbar-white');
+            $('.navbar').removeClass('navbar-transparent');
+            $('.navbar-brand').addClass('logo-transparent');
+            $('.navbar-brand').removeClass('logo');
+            $('.nav-item a').addClass('black-text');
+            $('.nav-item a').removeClass('white-text');
+            $('.nav-link i').addClass('black-text');
+            $('.nav-link i').removeClass('white-text');
+        }
+    });
+})
