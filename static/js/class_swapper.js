a8d86f13f1bfdf5747a18632d5309486485b12ea
Peter Krivan <krivan.peter@outlook.com>
2019-05-10 07:38:00 +0000

    bug fixed, when after resize to small screen navbar changes style

--- static/js/class_swapper.js
+++ static/js/class_swapper.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
     changeNavBar();
 })
 /*If window resized, run the ,,changeNavBar()" function*/
-$(window).resize(function () { 
+$(window).resize(function() {
     changeNavBar();
 });
 
 /*Function to change the navbar behavior depends on the size of the screen*/
 function changeNavBar() {
-    if ($(window).width() > 768) {
-        $(document).scroll(function() {
+    $(document).on("scroll", function() {
+        if ($(window).width() > 767) {
             if ($(window).scrollTop() === 0) {
                 $('.navbar').addClass('navbar-transparent');
                 $('.navbar').removeClass('navbar-white');
@@ -22,15 +22,25 @@ function changeNavBar() {
                 $('.nav-link i').removeClass('black-text');
             }
             else {
-                $('.navbar').addClass('navbar-white');
                 $('.navbar').removeClass('navbar-transparent');
-                $('.navbar-brand').addClass('logo-transparent');
+                $('.navbar').addClass('navbar-white');
                 $('.navbar-brand').removeClass('logo');
-                $('.nav-item a').addClass('black-text');
+                $('.navbar-brand').addClass('logo-transparent');
                 $('.nav-item a').removeClass('white-text');
-                $('.nav-link i').addClass('black-text');
+                $('.nav-item a').addClass('black-text');
                 $('.nav-link i').removeClass('white-text');
+                $('.nav-link i').addClass('black-text');
             }
-        });
-    }
+        }
+        else {
+            $('.navbar').addClass('navbar-transparent');
+            $('.navbar').removeClass('navbar-white');
+            $('.navbar-brand').addClass('logo');
+            $('.navbar-brand').removeClass('logo-transparent');
+            $('.nav-item a').addClass('white-text');
+            $('.nav-item a').removeClass('black-text');
+            $('.nav-link i').addClass('white-text');
+            $('.nav-link i').removeClass('black-text');
+        }
+    });
 }
