ff8cb5c8bd8c1a7afccb3fc42264df737c8dcc39
krivan.peter <44240979+krivanpeter@users.noreply.github.com>
2019-05-19 19:49:07 +0100

    secret key changed and added to env.py which is in gitignore

--- attractor/settings.py
+++ attractor/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n!!%svck*v3($!$9xmd5)w8lv)$c@z6n%v%al572f334fvkk_t'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
