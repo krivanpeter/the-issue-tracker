da77320c1db257992bd9e340cbf8401fc410f76e
krivan.peter <44240979+krivanpeter@users.noreply.github.com>
2019-05-25 09:50:59 +0100

    pagination added to 'news' page. Css for news edited

--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import New
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 '''
@@ -8,7 +9,17 @@ A view which shows all the news
 '''
 @login_required
 def all_news(request):
-    news = New.objects.all()
-    return render(request, "news.html", {"news": news})
+    news_list = New.objects.all()
+    page = request.GET.get('page', 1)
+    paginator = Paginator(news_list, 5)
+
+    try:
+        news = paginator.page(page)
+    except PageNotAnInteger:
+        news = paginator.page(1)
+    except EmptyPage:
+        news = paginator.page(paginator.num_pages)
+
+    return render(request, 'news.html', {'news': news})
 
 
