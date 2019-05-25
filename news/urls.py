840d4ed4b9f8c09f468bed0133c480873424f1db
krivan.peter <44240979+krivanpeter@users.noreply.github.com>
2019-05-25 15:56:18 +0100

    one new of news now opens on a new page

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import all_news
+from .views import all_news, new_detail
 
 urlpatterns = [
     url(r'^$', all_news, name="news"),
+    url(r'^(?P<pk>\d+)/$', new_detail, name="new_detail")
     ]
--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import New
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -9,7 +9,7 @@ A view which shows all the news
 '''
 @login_required
 def all_news(request):
-    news_list = New.objects.all()
+    news_list = New.objects.all().order_by('-published_date')
     page = request.GET.get('page', 1)
     paginator = Paginator(news_list, 5)
 
@@ -23,3 +23,11 @@ def all_news(request):
     return render(request, 'news.html', {'news': news})
 
 
+'''
+A view which returs a single New object based on the ID(pk)
+'''
+def new_detail(request, pk):
+    new = get_object_or_404(New, pk=pk)
+    new.views += 1
+    new.save()
+    return render(request, "newdetail.html", {'new':new})
