1212693183edce03115a2e7faa332fa93332aefd
krivan.peter <44240979+krivanpeter@users.noreply.github.com>
2019-06-20 12:33:24 +0100

    'user' extended with gender and avatar. Called: UserProfile. It is automaticly generated when user register

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='profile')
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    avatar = models.ImageField()
+
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+post_save.connect(create_profile, sender=User)
--- bugs/views.py
+++ bugs/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .models import Bug
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 
 @login_required
 def all_bugs(request):
+    users = User.objects.all()
     bugs = Bug.objects.all()
-    return render(request, "bugs.html", {"bugs": bugs})
+    return render(request, "bugs.html", {"bugs": bugs, "users": users})
