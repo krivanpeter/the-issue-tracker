271fd0e86c65e6801208d6862be5d390fda185a8
krivan.peter <44240979+krivanpeter@users.noreply.github.com>
2019-06-24 07:50:52 +0100

    Bugs now show the uploader's details

--- bugs/models.py
+++ bugs/models.py
@@ -1,19 +1,18 @@
 from django.db import models
 from django.utils import timezone
-
+from accounts.models import UserProfile
 
 '''
 A piece of Bug
 '''
 class Bug(models.Model):
     title = models.CharField(max_length=254, default="")
-    reported_by = models.CharField(max_length=254, default="")
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
     views = models.IntegerField(default=0)
     upvotes = models.IntegerField(default=0)
-    image = models.ImageField(upload_to="img", blank=True, null=True)
+    reported_by = models.ForeignKey(UserProfile, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
--- bugs/views.py
+++ bugs/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 from .models import Bug
-from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 
 @login_required
 def all_bugs(request):
-    users = User.objects.all()
     bugs = Bug.objects.all()
-    return render(request, "bugs.html", {"bugs": bugs, "users": users})
+    return render(request, "bugs.html", {"bugs": bugs})
