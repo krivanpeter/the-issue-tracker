0e70d864868b766ff1bd1261a303b39eec721492
krivan.peter <44240979+krivanpeter@users.noreply.github.com>
2019-06-28 10:19:09 +0100

    Style added to Comments, position of comments fixed on large and small screens

--- comments/forms.py
+++ comments/forms.py
@@ -5,6 +5,11 @@ class CommentForm(forms.Form):
     content_type = forms.CharField(widget=forms.HiddenInput)
     object_id = forms.IntegerField(widget=forms.HiddenInput)
     parent_id = forms.IntegerField(widget=forms.HiddenInput, required=False)
-    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write a comment...', 'rows': '3'}), label='')
+    content = forms.CharField(widget=forms.Textarea(
+        attrs={
+            'class': 'form-control comment-input',
+            'placeholder': 'Write a comment...',
+            'rows': '3'}),
+        label='')
 
 
