53fa3a31d8e0f4905e6951b24a80fd8015ccf916
krivan.peter <44240979+krivanpeter@users.noreply.github.com>
2019-08-25 11:22:58 +0100

    style and designadded to cart page for better visibility

--- cart/views.py
+++ cart/views.py
@@ -14,7 +14,7 @@ def add_to_cart(request, id):
         cart = request.session.get('cart', {})
         cart[id] = cart.get(id, quantity)
         request.session['cart'] = cart
-        return redirect(reverse('all_packages'))
+        return redirect(reverse('view_cart'))
     else:
         return redirect('all_packages')
 
