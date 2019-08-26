b0e8aa6a1232b6a35667bd9c2421fb86c4d62084
krivan.peter <44240979+krivanpeter@users.noreply.github.com>
2019-08-26 10:17:17 +0100

    forms created for checkout

---
+++ checkout/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import Order
+
+
+class MakePaymentForm(forms.ModelForm):
+    # Payment Form
+    MONTH_CHOICES = [(i, i) for i in range(1, 12)]
+    YEAR_CHOICES = [(i, i) for i in range(2017, 2036)]
+
+    credit_card_number = forms.CharField(label='Credit card number', required=False)
+    cvv = forms.CharField(label='Security Code (CVV)', required=False)
+    expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=False)
+    expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False)
+    stripe_id = forms.CharField(widget=forms.HiddenInput)
+
+
+class OrderForm(forms.Form):
+    # Order Form
+    class Meta:
+        model = Order
+        fields = (
+            'full_name',
+            'phone_number',
+            'country',
+            'postcode',
+            'town_or_city',
+            'street_address1',
+            'street_address2',
+            'country')
